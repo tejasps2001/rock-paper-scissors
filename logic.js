36a853dfe18518919a029829a97af33920ffdd81
Tejas P S <tejasps2001@gmail.com>
2023-08-13 16:10:55 +0530

    Reset score after five matches

--- logic.js
+++ logic.js
@@ -7,31 +7,55 @@
 let compScore = 0; 
 let playerScore = 0;
 
-let playerChoice = '';
 
-function getPlay(button) {
-    button.addEventListener('click', e => {
-        playerChoice = e.target.innerText;
-    });
+function play(e) {
+    let playerChoice = '';
+    playerChoice = e.target.innerText;
+    let compChoice = getCompPlay();
+    console.log(decideWinner(playerChoice, compChoice))
+    if (playerScore + compScore == 5) {
+        getGameWinner();
+        compScore = 0;
+        playerScore = 0;
+    }
 }
 
-const buttons = document.querySelectorAll('button');
-
-// Get user's play from button press.
-buttons.forEach(getPlay);
-
-if (playerScore > compScore) {
-    console.log('Whooo! You won!');
+function getCompPlay() {
+    choice = Math.floor(Math.random() * 3);
+    switch(choice) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
 }
-else if(compScore > playerScore) {
-    console.log('Bummer! You lost!');
-}
-else {
-    console.log("It's a tie!");
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+// Get user's play from button press
+rock.addEventListener('click', play);
+paper.addEventListener('click', play);
+scissors.addEventListener('click', play);
+
+function getGameWinner() {
+    if (playerScore > compScore) {
+        console.log('Whooo! You won!');
+    }
+    else if(compScore > playerScore) {
+        console.log('Bummer! You lost!');
+    }
+    else {
+        console.log("It's a tie!");
+    }
 }
 
 function decideWinner(player, comp) {
     if(player == comp) {
+        console.log(player, "doesn't do anything to", comp);
         console.log("It's a tie!")
         return 'tie';
     }
